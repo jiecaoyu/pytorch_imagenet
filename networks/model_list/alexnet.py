e659d46e13eb50a950be7a9e1c7c051d740e8f55
jiecaoyu <jiecaoyu@umich.edu>
2017-09-04 00:49:05 -0400

    write LRN layers for AlexNet

--- networks/model_list/alexnet.py
+++ networks/model_list/alexnet.py
@@ -4,6 +4,34 @@ import torch.utils.model_zoo as model_zoo
 
 __all__ = ['AlexNet', 'alexnet']
 
+class LRN(nn.Module):
+    def __init__(self, local_size=1, alpha=1.0, beta=0.75, ACROSS_CHANNELS=True):
+        super(LRN, self).__init__()
+        self.ACROSS_CHANNELS = ACROSS_CHANNELS
+        if ACROSS_CHANNELS:
+            self.average=nn.AvgPool3d(kernel_size=(local_size, 1, 1),
+                    stride=1,
+                    padding=(int((local_size-1.0)/2), 0, 0))
+        else:
+            self.average=nn.AvgPool2d(kernel_size=local_size,
+                    stride=1,
+                    padding=int((local_size-1.0)/2))
+        self.alpha = alpha
+        self.beta = beta
+
+
+    def forward(self, x):
+        if self.ACROSS_CHANNELS:
+            div = x.pow(2).unsqueeze(1)
+            div = self.average(div).squeeze(1)
+            div = div.mul(self.alpha).add(1.0).pow(self.beta)
+        else:
+            div = x.pow(2)
+            div = self.average(div)
+            div = div.mul(self.alpha).add(1.0).pow(self.beta)
+        x = x.div(div)
+        return x
+
 class AlexNet(nn.Module):
 
     def __init__(self, num_classes=1000):
@@ -11,11 +39,11 @@ class AlexNet(nn.Module):
         self.features = nn.Sequential(
             nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(96, eps=1e-4, momentum=0.1, affine=True),
+            LRN(local_size=5, alpha=0.0001, beta=0.75),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(96, 256, kernel_size=5, padding=2, groups=2),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(256, eps=1e-4, momentum=0.1, affine=True),
+            LRN(local_size=5, alpha=0.0001, beta=0.75),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(256, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
